5b7ff3ace6fa16d56487c32aea67616336621525
Vargath <jrtvgz@gmail.com>
2024-10-31 17:15:08 +0100

    Ajustar dependencias y agregar gitignore

--- csharp-clean-architecture-demo/2 - Services/obj/Debug/net8.0/2 - Services.AssemblyInfo.cs
+++ csharp-clean-architecture-demo/2 - Services/obj/Debug/net8.0/2 - Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2 - Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e461a2cf9afca3878f0d42b5ed728696d5ef9cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae684b0a942bead96390797daf350acc11d71210")]
 [assembly: System.Reflection.AssemblyProductAttribute("2 - Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2 - Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- csharp-clean-architecture-demo/4 - Console/obj/Debug/net8.0/4 - Console.AssemblyInfo.cs
+++ csharp-clean-architecture-demo/4 - Console/obj/Debug/net8.0/4 - Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4 - Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e461a2cf9afca3878f0d42b5ed728696d5ef9cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae684b0a942bead96390797daf350acc11d71210")]
 [assembly: System.Reflection.AssemblyProductAttribute("4 - Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4 - Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
